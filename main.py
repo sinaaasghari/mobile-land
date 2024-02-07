067acf838e845dacd11a33b03e5735b847a3cbb5
kiarash <33625174+krsam99@users.noreply.github.com>
2024-02-07 11:38:24 +0330

    testing functionality for 20 links
    it looks like the code is working fine. need to check for 20 random links

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from Scrapper import Scrapper
+import pandas as pd
 
 if __name__ == '__main__':
-    s24_data = Scrapper.scrape_phone_data('https://www.gsmarena.com/samsung_galaxy_s24_ultra-12771.php')
-    print(s24_data)
+    file = open('kiarash-links.txt', 'r')
+    links = str.split(file.read(), '\n')
+    phones_data = []
+    for i in range(20):
+        phones_data.append(Scrapper.scrape_phone_data(links[i]))
+    df = pd.DataFrame(phones_data)
+    df.to_csv('test_data.csv')
+    file.close()
