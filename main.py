27522ac35a65310876ce69de5afef57b4bea4ca1
parsa <141657984+p-abdolmaleki@users.noreply.github.com>
2024-02-07 18:16:19 +0330

    scraping parsa datas, ordering test and data folder, ceating sample notebook to guide data scraping

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from Scrapper import Scrapper
+from multiprocessing.dummy import Pool as ThreadPool
 import pandas as pd
-from threading import Thread
-from time import sleep
 
 
 if __name__ == '__main__':
-    file = open('kiarash-links.txt', 'r')
-    links = str.split(file.read(), '\n')
-    phones_data = []
-    for i in range(20):
-        
-        like_name = links[i][24:]
-        print(f' initialing {like_name}')
-        
-        t = Thread(target=Scrapper.scrape_phone_data , args=(links[i] , phones_data) , name=like_name)
-        t.start()
-        
-        sleep(2)
-        
-    df = pd.DataFrame(phones_data)
-    df.to_csv('test_data.csv')
-    file.close()
+    links = list()
+    with open('links/kiarash-links.txt', 'r') as file:
+        for row in file.readlines():
+            links.append(row.replace('\n', ''))
+
+    pool = ThreadPool(7)
+    phone_details = pool.map(Scrapper.scrape_phone_data, links[:20])
+    pool.close()
+    pool.join()
+
+    df = pd.DataFrame(phone_details)
+    df.to_csv('tests/test_data_with_multiThread_scrap.csv')
