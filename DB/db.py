423021e250849c8a9e2b5b26c5853a88d2aafebd
kiarash <33625174+krsam99@users.noreply.github.com>
2024-02-08 22:57:05 +0330

    feat db can be created now.
    minor changes to datas and changed directories in order to have access to datas.

--- DB/db.py
+++ DB/db.py
@@ -9,7 +9,7 @@ connection_url = engine.URL.create(
     username='root',
     password='',
     host='localhost',
-    database='',
+    database='gsmarena_data',
 )
 
 engine = create_engine(connection_url)
@@ -21,7 +21,7 @@ class Phone(Base):
     __tablename__ = 'phone'
     id = Column(Integer, primary_key=True, autoincrement=True)
     brand_id = Column(Integer, ForeignKey('brand.id'))
-    name = Column(String(128))
+    model = Column(String(128))
     year = Column(Integer)
     price = Column(Integer)
     Battery = Column(Integer)
@@ -30,10 +30,13 @@ class Phone(Base):
     weight = Column(Integer)
     sim = Column(String(16))
     size = Column(Integer)
-    resolution = Column(String(16))
+    length_resolution = Column(Integer)
+    width_resolution = Column(Integer)
     ppi = Column(Integer)
     ratio = Column(DECIMAL)
-    dimension = Column(String(16))
+    length = Column(DECIMAL)
+    width = Column(DECIMAL)
+    thickness = Column(DECIMAL)
 
 
 class Brand(Base):
@@ -78,20 +81,18 @@ class Phone_Memory(Base):
 
 Base.metadata.create_all(engine)
 
+phone = pd.read_csv('db_datas/phone.csv')
+os = pd.read_csv('db_datas/os.csv')
+brand = pd.read_csv('db_datas/brand.csv')
+sensor = pd.read_csv('db_datas/sensor.csv')
+phone_sensor = pd.read_csv('db_datas/phone-sensor.csv')
+memory = pd.read_csv('db_datas/memory.csv')
+phone_memory = pd.read_csv('db_datas/phone-memory.csv')
 
-phone = pd.read_csv('phone_data.csv')
-os = pd.read_csv('os_data.csv')
-brand = pd.read_csv('brand_data.csv')
-sensor = pd.read_csv('sensor_data.csv')
-phone_sensor = pd.read_csv('phone_sensor_data.csv')
-memory = pd.read_csv('memory.csv')
-phone_memory = pd.read_csv('phone_memory_data.csv')
-
-
-phone.to_sql('phone', con=engine, if_exists='append', index=False)
-os.to_sql('os', con=engine, if_exists='append', index=False)
 brand.to_sql('brand', con=engine, if_exists='append', index=False)
 sensor.to_sql('sensor', con=engine, if_exists='append', index=False)
-phone_sensor.to_sql('phone_sensor', con=engine, if_exists='append', index=False)
+os.to_sql('os', con=engine, if_exists='append', index=False)
 memory.to_sql('memory', con=engine, if_exists='append', index=False)
+phone.to_sql('phone', con=engine, if_exists='append', index=False)
+phone_sensor.to_sql('phone_sensor', con=engine, if_exists='append', index=False)
 phone_memory.to_sql('phone_memory', con=engine, if_exists='append', index=False)
